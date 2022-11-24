ef7d4463f0aa985dfee62e163b2995dbfb87150d
brianegona <113472988+brianegona@users.noreply.github.com>
2022-11-23 20:49:24 -0500

    Revert "added FindAnglesInTriangleAndPrint() function"

--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,7 +1,2 @@
 #pragma once
-
-#define DEGREECONVERSION	(180 / 3.1415)
-#define DEGREETOTAL			180
-
 char* analyzeTriangle(int side1, int side2, int side3);
-void FindAnglesInTriangleAndPrint(double side1, double side2, double side3);
