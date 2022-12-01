6ff7f72f5186c9be2938e60aedf83c33b55e8305
hsihak <sihak.hs@gmail.com>
2022-12-01 16:45:41 -0500

    Add more test cases for RectangleCheck Function

--- RectangleTests/RectangleTests.cpp
+++ RectangleTests/RectangleTests.cpp
@@ -6,7 +6,10 @@
 extern "C" double FindArea(double, double, double, double);
 extern "C" double FindPerimeter(double, double, double, double);
 extern "C" struct POINT { int x, y; };
+extern "C" POINT AssignPoints(int x, int y);
 extern "C" bool ArePointsDuplicated(POINT, POINT, POINT, POINT);
+extern "C" double FindDiagonalOfNonRightTriangle(POINT, POINT);
+extern "C" double FindHypotenuseOfRightTriangle(double, double);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -176,4 +179,134 @@ namespace RectangleTests
 			Assert::IsFalse(ArePointsDuplicated(point1, point2, point3, point4));
 		};
 	};
+
+
+	TEST_CLASS(AssignPointsFunctionality)
+	{
+	public:
+
+		TEST_METHOD(ValidateAssignPointsTest1)
+		{
+			// Initialize points with positive dummy values
+			// This is testing to validate if the a point with x, y coordinate are assign to POINT struct and return the correct result
+			POINT expectedPoints;
+			expectedPoints.x = 5;
+			expectedPoints.y = 2;
+
+			POINT actualPoints = AssignPoints(5, 2);
+
+			Assert::AreEqual(expectedPoints.x, actualPoints.x);
+			Assert::AreEqual(expectedPoints.y, actualPoints.y);
+		};
+
+		TEST_METHOD(ValidateAssignPointsTest2)
+		{
+			// Initialize points with negative dummy values
+			// This is testing to validate if the a point with x, y coordinate are assign to POINT struct and return the correct result
+			POINT expectedPoints;
+			expectedPoints.x = -2;
+			expectedPoints.y = -1;
+
+			POINT actualPoints = AssignPoints(-2, -1);
+
+			Assert::AreEqual(expectedPoints.x, actualPoints.x);
+			Assert::AreEqual(expectedPoints.y, actualPoints.y);
+		};
+
+		TEST_METHOD(ValidateAssignPointsTest3)
+		{
+			// Initialize points with negative value on y coordinate and positive value on x coordinate dummy values
+			// This is testing to validate if the a point with x, y coordinate are assign to POINT struct and return the correct result
+			POINT expectedPoints;
+			expectedPoints.x = -23;
+			expectedPoints.y = 91;
+
+			POINT actualPoints = AssignPoints(-23, 91);
+
+			Assert::AreEqual(expectedPoints.x, actualPoints.x);
+			Assert::AreEqual(expectedPoints.y, actualPoints.y);
+		};
+
+		TEST_METHOD(ValidateAssignPointsTest4)
+		{
+			// Initialize points with positive value on x coordinate and negative value on y coordinate dummy values
+			// This is testing to validate if the a point with x, y coordinate are assign to POINT struct and return the correct result
+			POINT expectedPoints;
+			expectedPoints.x = 12;
+			expectedPoints.y = -3;
+
+			POINT actualPoints = AssignPoints(12, -3);
+
+			Assert::AreEqual(expectedPoints.x, actualPoints.x);
+			Assert::AreEqual(expectedPoints.y, actualPoints.y);
+		}
+	};
+
+	TEST_CLASS(FindDiagonalOfNonRightTriangleFunctionality)
+	{
+	public:
+
+		TEST_METHOD(FindDiagonalOfNonRightTriangleTest1)
+		{
+			// Initialize pointA and B (x,y coordinate) with dummy values
+			// This is testing to validate the function calculate Diagonal of a triangle of a rectangle return the correct result
+			POINT pointA, pointB;
+
+			pointA.x = -3;
+			pointA.y = 2;
+
+			pointB.x = 4;
+			pointB.y = 2;
+
+			double expectedResult = 7.000000;
+			double actualResult = FindDiagonalOfNonRightTriangle(pointA, pointB);
+
+			Assert::AreEqual(expectedResult, actualResult);
+		};
+
+		TEST_METHOD(FindDiagonalOfNonRightTriangleTest2)
+		{
+			// Initialize pointC and D (x,y coordinate) with dummy values
+			// This is testing to validate the function calculate Diagonal of a triangle of a rectangle return the correct result
+			POINT pointC, pointD;
+
+			pointC.x = -3;
+			pointC.y = -3;
+
+			pointD.x = 4;
+			pointD.y = -3;
+
+			double expectedResult = 7.000000;
+			double actualResult = FindDiagonalOfNonRightTriangle(pointC, pointD);
+
+			Assert::AreEqual(expectedResult, actualResult);
+		};
+	};
+
+	TEST_CLASS(FindHypotenuseOfRightTriangleFunctionality)
+	{
+	public:
+
+		TEST_METHOD(FindHypotenuseOfRightTriangleTriangleTest1)
+		{
+			// This is testing to validate the function calculate the hypotenuse of a triangle of a rectangle return the correct result
+			// dummy values are passed to the function to calculate the hypotenuse of a triangle
+
+			double expectedResult = 6.7082039324993694;
+			double actualResult = FindHypotenuseOfRightTriangle(6.00, 3.00);
+
+			Assert::AreEqual(expectedResult, actualResult);
+		};
+
+		TEST_METHOD(FindHypotenuseOfRightTriangleTriangleTest2)
+		{
+			// This is testing to validate the function calculate the hypotenuse of a triangle of a rectangle return the correct result
+			// dummy values are passed to the function to calculate the hypotenuse of a triangle
+			double expectedResult = 65.783512372022216;
+			double actualResult = FindHypotenuseOfRightTriangle(54.31, 37.12);
+
+			Assert::AreEqual(expectedResult, actualResult);
+		};
+	};
+
 }
